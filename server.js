f6f581c8457d655646e85bfdbe02ed30f4ec97b5
natesanow <152323081+natesanow@users.noreply.github.com>
2024-03-01 15:37:07 -0800

    added code to server js file

--- server.js
+++ server.js
@@ -14,4 +14,14 @@ app.use('/api', api);
 
 app.use(express.static('public'));
 
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
 
+app.get('/notes,', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App is listening at http://localhost:${PORT}`)
+);
